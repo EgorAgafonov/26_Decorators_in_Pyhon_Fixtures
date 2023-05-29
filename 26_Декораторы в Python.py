6b81fdafaa23e278425ca27701cc4bff18fcf5fc
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-05-29 18:40:39 +0300

    File updated

--- 26_Декораторы в Python.py
+++ 26_Декораторы в Python.py
@@ -94,22 +94,22 @@ from decorators import do_twice
 
 # 2
 
-def working_hours(func):
-    def wrapper():
-        if 9 <= datetime.datetime.now().hour < 18:
-            func()
-        else:
-            print("Я не пишу тесты на python, время отдыха (:-)!")
-    return wrapper
-
-@ working_hours
-def writing_tests():
-    print("Я пишу тесты на python!")
+# def working_hours(func):
+#     def wrapper():
+#         if 9 <= datetime.datetime.now().hour < 18:
+#             func()
+#         else:
+#             print("Я не пишу тесты на python, время отдыха (:-)!")
+#     return wrapper
+#
+# @ working_hours
+# def writing_tests():
+#     print("Я пишу тесты на python!")
 
 # writing_tests = working_hours(writing_tests)
 
 
-writing_tests()
+# writing_tests()
 
 # 3
 
@@ -139,3 +139,28 @@ writing_tests()
 #
 #
 # test_twice()
+
+# 5
+
+def pipes_replacement(func):
+    def wrapper():
+        if 9 < datetime.datetime.now().hour < 19:
+            func()
+        else:
+            print('Профилактические работы не ведутся.')
+    return wrapper
+
+
+@pipes_replacement
+def prevention_warning():
+    print('Ведутся профилактические работы.')
+
+
+prevention_warning()
+
+# def prevention_warning():
+#     print('Ведутся профилактические работы.')
+#
+# prevention_warning = pipes_replacement(prevention_warning)
+#
+# prevention_warning()
