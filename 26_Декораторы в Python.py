e7e20c9c6a38cb295c761bea8557460f7f5de5d5
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-07-27 22:11:49 +0300

    Files updated

--- 26_Декораторы в Python.py
+++ 26_Декораторы в Python.py
@@ -228,23 +228,23 @@ import datetime
 
 # 2  Фикстуры setup и teardown в библиотеке pytests.
 
-# @pytest.fixture()
-# def get_key():
-#     # переменные email и password нужно заменить своими учетными данными
-#     response = requests.post(url='https://petfriends.skillfactory.ru/login',
-#                              data={"email": email, "pass": password})
-#     assert response.status_code == 200, 'Запрос выполнен неуспешно'
-#     assert 'Cookie' in response.request.headers, 'В запросе не передан ключ авторизации'
-#     return response.request.headers.get('Cookie')
-#
-#
-# def test_getAllPets(get_key):  # передав в аргумент фикстуру get_key мы избавили себя от необходимости
-#     # писать в теле тестовой функции test_getAllPets кусок кода с повторным предварительном получением API-(TOKEN)ключа.
-#     # Это пример использования декораторов-фикстур при тестировании API платформ с использ-ем библ-ки pytests.
-#     response = requests.get(url='https://petfriends.skillfactory.ru/api/pets',
-#                             headers={"Cookie": get_key})
-#     assert response.status_code == 200, 'Запрос выполнен неуспешно'
-#     assert len(response.json().get('pets')) > 0, 'Количество питомцев не соответствует ожиданиям'
+@pytest.fixture()
+def get_key():
+    # переменные email и password нужно заменить своими учетными данными
+    response = requests.post(url='https://petfriends.skillfactory.ru/login',
+                             data={"email": email, "pass": password})
+    assert response.status_code == 200, 'Запрос выполнен неуспешно'
+    assert 'Cookie' in response.request.headers, 'В запросе не передан ключ авторизации'
+    return response.request.headers.get('Cookie')
+
+
+def test_getAllPets(get_key):  # передав в аргумент фикстуру get_key мы избавили себя от необходимости
+    # писать в теле тестовой функции test_getAllPets кусок кода с повторным предварительном получением API-(TOKEN)ключа.
+    # Это пример использования декораторов-фикстур при тестировании API платформ с использ-ем библ-ки pytests.
+    response = requests.get(url='https://petfriends.skillfactory.ru/api/pets',
+                            headers={"Cookie": get_key})
+    assert response.status_code == 200, 'Запрос выполнен неуспешно'
+    assert len(response.json().get('pets')) > 0, 'Количество питомцев не соответствует ожиданиям'
 #
 #
 # @pytest.fixture(autouse=True)
@@ -313,36 +313,36 @@ import datetime
 #         print(request_fixture)
 
 # 3
-@pytest.fixture(scope="class")
-def get_key(request):
-    # переменные email и password нужно заменить своими учетными данными
-    response = requests.post(url='https://petfriends.skillfactory.ru/login',
-                             data={"email": email, "pass": password})
-    assert response.status_code == 200, 'Запрос выполнен неуспешно'
-    assert 'Cookie' in response.request.headers, 'В запросе не передан ключ авторизации'
-    print("\nreturn auth_key")
-    return response.request.headers.get('Cookie')
-
-
-@pytest.fixture(autouse=True)
-def request_fixture(request):
-    if 'Pets' in request.function.__name__:
-        print(f"\nЗапущен тест из сьюта Дом Питомца: {request.function.__name__}")
-
-
-class TestClassPets:
-
-    def test_getAllPets2(self, get_key):
-        response = requests.get(url='https://petfriends.skillfactory.ru/api/pets',
-                                headers={"Cookie": get_key})
-        assert response.status_code == 200, 'Запрос выполнен неуспешно'
-        assert len(response.json().get('pets')) > 0, 'Количество питомцев не соответствует ожиданиям'
-
-    def test_getMyPets2(self, get_key):
-        response = requests.get(url='https://petfriends.skillfactory.ru/my_pets',
-                                headers={"Cookie": get_key})
-        assert response.status_code == 200, 'Запрос выполнен неуспешно'
-        assert response.headers.get('Content-Type') == 'text/html; charset=utf-8'
-
-    def test_anotherTest(self):
-        pass
+# @pytest.fixture(scope="class")
+# def get_key(request):
+#     # переменные email и password нужно заменить своими учетными данными
+#     response = requests.post(url='https://petfriends.skillfactory.ru/login',
+#                              data={"email": email, "pass": password})
+#     assert response.status_code == 200, 'Запрос выполнен неуспешно'
+#     assert 'Cookie' in response.request.headers, 'В запросе не передан ключ авторизации'
+#     print("\nreturn auth_key")
+#     return response.request.headers.get('Cookie')
+#
+#
+# @pytest.fixture(autouse=True)
+# def request_fixture(request):
+#     if 'Pets' in request.function.__name__:
+#         print(f"\nЗапущен тест из сьюта Дом Питомца: {request.function.__name__}")
+#
+#
+# class TestClassPets:
+#
+#     def test_getAllPets2(self, get_key):
+#         response = requests.get(url='https://petfriends.skillfactory.ru/api/pets',
+#                                 headers={"Cookie": get_key})
+#         assert response.status_code == 200, 'Запрос выполнен неуспешно'
+#         assert len(response.json().get('pets')) > 0, 'Количество питомцев не соответствует ожиданиям'
+#
+#     def test_getMyPets2(self, get_key):
+#         response = requests.get(url='https://petfriends.skillfactory.ru/my_pets',
+#                                 headers={"Cookie": get_key})
+#         assert response.status_code == 200, 'Запрос выполнен неуспешно'
+#         assert response.headers.get('Content-Type') == 'text/html; charset=utf-8'
+#
+#     def test_anotherTest(self):
+#         pass
